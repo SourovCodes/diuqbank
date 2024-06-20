0ea3e8149f4970f219e5d3fb62e262e1585d4fa7
Sourov Biswas <sourov2305101004@diu.edu.bd>
2024-06-20 11:35:32 +0530

    frontend layout

---
+++ resources/views/layouts/web-layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+
+    <meta name="application-name" content="{{ config('app.name') }}">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <script data-cfasync="false">
+        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia(
+            '(prefers-color-scheme: dark)').matches)) {
+            document.documentElement.classList.add('dark');
+
+        } else {
+            document.documentElement.classList.remove('dark')
+
+        }
+    </script>
+
+    <title>{{ config('app.name') }}</title>
+
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+
+    @filamentStyles
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-slate-100 text-gray-900 dark:text-gray-100 dark:bg-slate-800 antialiased flex flex-col min-h-screen">
+@include('inc.navbar')
+{{ $slot }}
+
+@livewire('notifications')
+
+@filamentScripts
+@vite('resources/js/app.js')
+</body>
+</html>
