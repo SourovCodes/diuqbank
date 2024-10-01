c158783083e9ee646df958d9c12e8a508403beb7
Sourov Biswas <itsourov12@gmail.com>
2024-10-02 02:44:35 +0600

    notice

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,5 +1,13 @@
 <x-impersonate::banner/>
 
+<div class="bg-indigo-900 text-center py-2 lg:px-4">
+	<a href="https://www.facebook.com/groups/1298831488164950" target="_blank" class="p-2 bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+	  <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">New</span>
+	  <span class="font-semibold mr-2 text-left flex-auto">Join our facebook group</span>
+	  <svg class="fill-current opacity-75 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z"/></svg>
+	</a>
+  </div>
+
 <div class="hideInApp" x-data="{ offCanvasMenu: false }">
 
 	<div class="fixed inset-0 flex z-40 lg:hidden" x-cloak x-show="offCanvasMenu" role="dialog" aria-modal="true">
