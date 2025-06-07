0fb4f6ad3cdc6d56eef5d349b62f9a228738dd3f
Sourov <sourov2305101004@diu.edu.bd>
2025-06-07 11:38:08 +0600

    Enhance home page layout with hero section, how it works, stats, and features; add button component for consistent styling

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/questions', [HomeController::class,'index'])->name('questions.index');
+Route::get('/questions/upload', [HomeController::class,'index'])->name('questions.create');
