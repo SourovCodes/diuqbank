54860f21775e7511c2ce31ba1515022714c25f99
Sourov Biswas <sourovcodes@gmail.com>
2025-08-25 03:29:05 +0600

    feat: Add profile image upload script to Vite configuration
    - Included 'resources/js/profile-image.js' in the Vite configuration for asset management, enhancing the functionality related to user profile images.

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
-                'resources/js/question-form.js'
+                'resources/js/question-form.js',
+                'resources/js/profile-image.js'
             ],
             refresh: true,
         }),
