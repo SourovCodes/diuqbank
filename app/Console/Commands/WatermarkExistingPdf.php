ebfd2d8be15999605e1d53bb7f7855be56309374
Sourov Biswas <sourovcodes@gmail.com>
2025-08-27 20:51:13 +0600

    fix: Update error handling in ImportQuestions command to set question status to NEED_FIX for invalid PDF URLs, ensuring better data integrity

---
+++ app/Console/Commands/WatermarkExistingPdf.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\CompressAndWatermarkPdfJob;
+use App\Models\Question;
+use Illuminate\Console\Command;
+
+class WatermarkExistingPdf extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:watermark-existing-pdf';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $questions = Question::where('is_watermarked', false)->get();
+
+        foreach ($questions as $question) {
+          CompressAndWatermarkPdfJob::dispatch($question->id);
+        }
+    }
+}
