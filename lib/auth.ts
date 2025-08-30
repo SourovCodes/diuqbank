9f317ae360a05132975d12953cb3e345d2fbb2dc
Sourov Biswas <sourovcodes@gmail.com>
2025-08-30 21:35:32 +0600

    feat: add Google social authentication support
    - Integrated Google as a social provider in the authentication configuration.
    - Utilized environment variables for client ID and secret to enhance security.

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,10 @@ export const auth = betterAuth({
     database: drizzleAdapter(db, {
         provider: "mysql",
     }),
+    socialProviders: {
+        google: {
+            clientId: process.env.AUTH_GOOGLE_ID as string,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+        },
+    },
 });
