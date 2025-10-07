8abba63ebea7424ebf0b2216d2a64a220b87febf
Sourov Biswas <sourovcodes@gmail.com>
2025-10-07 20:16:29 +0600

    feat: Add Semester model, factory, seeder, and migration for semester management

---
+++ app/Models/Semester.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Semester extends Model
+{
+    /** @use HasFactory<\Database\Factories\SemesterFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+    ];
+}
