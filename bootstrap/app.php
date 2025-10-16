5d024e582bb2f5ae1d0551a53d60f3e613a8b858
Sourov Biswas <sourovcodes@gmail.com>
2025-10-16 10:19:39 +0600

    feat: integrate Sentry for error tracking and performance monitoring in Laravel application

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,6 +6,8 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
+use Sentry\Laravel\Integration;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -37,4 +39,6 @@ return Application::configure(basePath: dirname(__DIR__))
 
             return $response;
         });
+                Integration::handles($exceptions);
+
     })->create();
