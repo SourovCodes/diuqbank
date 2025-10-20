1588967ee383db805fa009a2433c7b5019740148
Sourov Biswas <sourovcodes@gmail.com>
2025-10-21 00:23:20 +0600

    feat: return structured user data using SessionUserResource in user endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\GoogleAuthController;
+use App\Http\Resources\SessionUserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
-    return $request->user();
+    return new SessionUserResource($request->user());
 })->middleware('auth:sanctum');
 
 // Google OAuth for SPA
