11ffef58e4cd6963f9e82128dfb7b86d7d45386d
Sourov Biswas <sourovcodes@gmail.com>
2025-10-21 01:36:10 +0600

    feat: add ContributorsController and ContributorResource for managing contributors

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\GoogleAuthController;
+use App\Http\Controllers\Api\ContributorsController;
 use App\Http\Controllers\Api\QuestionsController;
 use App\Http\Resources\SessionUserResource;
 use Illuminate\Http\Request;
@@ -15,6 +16,9 @@ Route::get('/questions', [QuestionsController::class, 'index']);
 Route::get('/questions/{question}', [QuestionsController::class, 'show']);
 Route::post('/questions', [QuestionsController::class, 'store'])->middleware('auth:sanctum');
 Route::put('/questions/{question}', [QuestionsController::class, 'update'])->middleware('auth:sanctum');
+
+Route::get('/contributors', [ContributorsController::class, 'index']);
+
 
 // Google OAuth for SPA
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->middleware('guest');
