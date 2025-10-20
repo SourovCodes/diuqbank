09add0f2ae235fa477ebba6de2608de0e2eacfea
Sourov Biswas <sourovcodes@gmail.com>
2025-10-21 00:41:26 +0600

    feat: add QuestionsController with index method for paginated question retrieval

---
+++ app/Http/Controllers/Api/QuestionsController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\QuestionResource;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $departmentId = $request->integer('department_id') ?: null;
+        $courseId = $request->integer('course_id') ?: null;
+        $semesterId = $request->integer('semester_id') ?: null;
+        $examTypeId = $request->integer('exam_type_id') ?: null;
+        $uploaderId = $request->integer('uploader') ?: null;
+
+        $query = Question::query()
+            ->published()
+            ->department($departmentId)
+            ->course($courseId)
+            ->semester($semesterId)
+            ->examType($examTypeId)
+            ->latest()
+            ->with(['department', 'semester', 'course', 'examType']);
+        $questions = $query->paginate(12)->withQueryString();
+
+        return QuestionResource::collection($questions);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Question $question)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Question $question)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Question $question)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\Auth\GoogleAuthController;
+use App\Http\Controllers\Api\QuestionsController;
 use App\Http\Resources\SessionUserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return new SessionUserResource($request->user());
 })->middleware('auth:sanctum');
+
+Route::get('/questions', [QuestionsController::class, 'index']);
 
 // Google OAuth for SPA
 Route::get('/auth/google/redirect', [GoogleAuthController::class, 'redirect'])->middleware('guest');
