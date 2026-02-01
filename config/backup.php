d8b52105d5bd6283e5d7d64973647ec0c041441f
Sourov Biswas <sourovcodes@gmail.com>
2026-02-01 11:42:53 +0600

    fix: update email address for backup notifications

--- config/backup.php
+++ config/backup.php
@@ -210,7 +210,7 @@ return [
         'notifiable' => \Spatie\Backup\Notifications\Notifiable::class,
 
         'mail' => [
-            'to' => 'your@example.com',
+            'to' => 'sourovcodes@gmail.com',
 
             'from' => [
                 'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
